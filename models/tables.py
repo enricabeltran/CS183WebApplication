677f648870b5db15646c028f21dab4fcc1cbbcc7
Sam Reha <sreha@ucsc.edu>
2015-02-10 20:55:49 -0800

    Started basic database tables

---
+++ models/tables.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from datetime import datetime
+import re
+import unittest
+
+db.define_table('restaurants',
+    Field('account_ID', unique=True),
+    Field('name'),
+    Field('email'),
+    Field('phone'),
+    Field('address'),                 # Should be reference to external table
+    Field('culinaryStyle'),
+    Field('priceRange'),
+    Field('menu'),                    # Should be reference to external table
+    Field('images'),                  # Should be reference to external table
+    Field('description', 'text'),
+    )
+
+db.define_table('users',
+    Field('account_ID', unique=True),
+    Field('name'),
+    Field('email'),
+    Field('phone'),
+    Field('paymentInformation'),      # Should be reference to external table
+    Field('address'),                 # Should be reference to external table
+    )
